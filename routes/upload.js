076399dbd171bf7ea1a5972475272755509e981b
Andres Alba <bryanalba92@gmail.com>
2019-08-29 01:28:37 -0500

    Asignar la imagen a un hospital, usuario o médico

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,15 @@
 var express = require('express');
 
 var fileUpload = require('express-fileupload');
+var fs = require('fs');
 
 
 
 var app = express();
+
+var Hospital = require('../models/hospital');
+var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 // default options
 app.use(fileUpload());
@@ -79,15 +84,142 @@ app.put('/:tipo/:id', (req, res, next) => {
                 errors: err
             });
         }
-        res.status(200).json({
-            ok: true,
-            mensaje: 'Archivo movido',
-            extencionArchivo: extencionArchivo
-        });
+
+        subirPorTipo(tipo, id, nombreArchivo, res);
+        // res.status(200).json({
+        //     ok: true,
+        //     mensaje: 'Archivo movido',
+        //     extencionArchivo: extencionArchivo
+        // });
 
     })
 
 
 });
+
+
+function subirPorTipo(tipo, id, nombreArchivo, res) {
+
+    if (tipo === 'usuarios') {
+        Usuario.findById(id, (err, usuario) => {
+
+            if (!usuario) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Usuario no existe',
+                    errors: { message: 'Usuario no existe' }
+                });
+            }
+
+            var pathViejo = './uploads/usuarios/' + usuario.img;
+            // Si existe, elimina la anterior
+
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo, err => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error al eliminar imagen anterior',
+                            path: pathViejo
+                        });
+                    }
+                });
+            }
+
+            usuario.img = nombreArchivo;
+            usuario.save((err, usuarioActualizado) => {
+
+                usuarioActualizado.password = ':)';
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de usuario actualizada',
+                    usuario: usuarioActualizado
+                });
+
+            });
+        });
+    }
+
+    if (tipo === 'medicos') {
+
+        Medico.findById(id, (err, medico) => {
+
+            if (!medico) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Medico no existe',
+                    errors: { message: 'Medico no existe' }
+                });
+            }
+
+            var pathViejo = './uploads/medicos/' + medico.img;
+            // Si existe, elimina la anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo, err => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error al eliminar imagen anterior',
+                            path: pathViejo
+                        });
+                    }
+                });
+            }
+
+            medico.img = nombreArchivo;
+            medico.save((err, medicoActualizado) => {
+
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de medico actualizada',
+                    medico: medicoActualizado
+                });
+
+            });
+        });
+
+    }
+
+    if (tipo === 'hospitales') {
+
+        Hospital.findById(id, (err, hospital) => {
+
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Hospital no existe',
+                    errors: { message: 'Hospital no existe' }
+                });
+            }
+
+            var pathViejo = './uploads/hospitales/' + hospital.img;
+            // Si existe, elimina la anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo, err => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error al eliminar imagen anterior',
+                            path: pathViejo
+                        });
+                    }
+                });
+            }
+
+            hospital.img = nombreArchivo;
+            hospital.save((err, hospitalActualizado) => {
+
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de hospital actualizada',
+                    hospital: hospitalActualizado
+                });
+
+            })
+        });
+
+    }
+
+}
 
 module.exports = app;
